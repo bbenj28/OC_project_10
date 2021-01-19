a99a1685fffd602144cf454c1e907f31a07eb4cb
Benjamin Breton <bbenjaminbreton@gmail.com>
2021-01-19 17:00:07 +0100

    📥 add CoreDataManager

---
+++ Reciplease/Model/CoreData/CoreDataManager.swift
@@ -0,0 +1,16 @@
+//
+//  CoreDataManager.swift
+//  Reciplease
+//
+//  Created by Benjamin Breton on 19/01/2021.
+//
+
+import Foundation
+import CoreData
+class CoreDataManager {
+    let stack: CoreDataStack
+    init(_ stack: CoreDataStack) {
+        self.stack = stack
+    }
+    
+}
