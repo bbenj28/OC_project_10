0d646e043d23ec18988f7d27d76c3ae4a693e793
Benjamin Breton <bbenjaminbreton@gmail.com>
2021-01-20 07:41:13 +0100

    📥 add JSONStructureDecoder

--- Reciplease/Model/RecipeService/RecipeService.swift
+++ Reciplease/Model/RecipeService/RecipeService.swift
@@ -40,19 +40,7 @@ class RecipeService {
     }
 }
 
-class JSONStructureDecoder {
-    func decode<T: Decodable>(_ data: Data?, completionHandler: (Result<T, Error>) -> Void) {
-        guard let data = data else {
-            completionHandler(.failure(ApplicationErrors.nodata))
-            return
-        }
-        guard let dataJson = try? JSONDecoder().decode(T.self, from: data) else {
-            completionHandler(.failure(ApplicationErrors.dataConformityLess))
-            return
-        }
-        completionHandler(.success(dataJson))
-    }
-}
+
 
 enum ApplicationErrors: Error, CustomStringConvertible, Equatable {
     
