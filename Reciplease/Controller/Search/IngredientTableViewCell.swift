b9b4af617d302cbc659f12b55fb17f6d9d4b032a
Benjamin Breton <bbenjaminbreton@gmail.com>
2021-01-20 09:18:12 +0100

    ✏️ create SearchView

---
+++ Reciplease/Controller/Search/IngredientTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  IngredientTableViewCell.swift
+//  Reciplease
+//
+//  Created by Benjamin Breton on 20/01/2021.
+//
+
+import UIKit
+
+class IngredientTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
