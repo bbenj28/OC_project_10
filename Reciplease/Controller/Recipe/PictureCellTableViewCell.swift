35e570c39dd71e706ad58753cb9bf359050c9357
Benjamin Breton <bbenjaminbreton@gmail.com>
2021-01-21 11:40:20 +0100

    ✏️ create Recipe view

---
+++ Reciplease/Controller/Recipe/PictureCellTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  PictureCellTableViewCell.swift
+//  Reciplease
+//
+//  Created by Benjamin Breton on 21/01/2021.
+//
+
+import UIKit
+
+class PictureCellTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
